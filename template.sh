8d305375ba2a92f5d11839c969be4ee4e0e54d99
Namrata <namratagentyal2003@gmail.com>
2026-01-15 21:54:58 +0530

    Folder Structure is created

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating directories using bash scripting
+mkdir -p src
+mkdir -p research
+
+# creating files using bash scripting
+touch src/__init__.py
+touch research/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Direcoties and files are created successfully."
